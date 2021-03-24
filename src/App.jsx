19610da48efbc37f630a29c178d7f48912f7e8b4
Mongker <levanmong.dola.99@gmail.com>
2021-03-24 16:38:11 +0700

    Mongker: khoi tao project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+/**
+ * Copyright 2020 present, Lê Văn Mong.
+ * All rights reserved.
+ * @author Mong_Le_Van on 09/09/2020
+ * @email: levanmong.dola.99@gmail.com
+ * @student-code: 68DCHT20091
+ * @university: UTT (Đại học Công Nghệ Giao Thông Vận Tải)
+ */
+
+import React from "react";
+
+// components
+
+
+function App() {
+    return (
+        <div>Mong</div>
+    );
+}
+
+export default React.memo(App);
